82138b170b7492924ef090c34efd20ec9d84eb9d
Rahad khan <clash146098@gmail.com>
2022-03-25 21:45:36 +0600

    json file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1 className="text-3xl">
       Hello world!
     </h1>
